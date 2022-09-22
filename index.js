02a91c9f046e68a853876a474ae9a2ffabed0fd2
Alexandra Iglesia <alexxandraiglesia@gmail.com>
2022-09-21 21:51:29 -0400

    commit tree f53aff98b78834f58f3b1cdb7a1846f4d7674bea

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-// code your solution here
+function saturdayFun(activity = "roller-skate"){
+    return `This Saturday, I want to ${activity}!`;
+}
+saturdayFun();
+saturdayFun("I want to bathe my dog");
+
+function mondayWork(activity = "go to the office"){
+    return `This Monday, I will ${activity}.`;
+}
+
+// function wrapAdjective(niceWords = "special"){
+//     const symbol = "*";
+//     return function() {
+//         console.log(`You are ${symbol} ${niceWords} ${symbol}!`);
+//     }
+// };
+// wrapAdjective()();
+
+function wrapAdjective(niceWords, msg = "You are") {
+    const innerFunction = function (symbol = "*") {
+        return `${msg} ${niceWords}${symbol}${niceWords}!`;
+    }
+    return innerFunction;
+}
